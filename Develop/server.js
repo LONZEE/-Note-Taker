5aae0f1ccc02588f8a46310e509b3500834d9274
LONZEE <128856310+LONZEE@users.noreply.github.com>
2024-04-16 19:50:50 -0500

    Added necessary routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,14 @@
-const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/apiRoutes');
-const PORT = process.env.PORT || 3001;
-const app = express();
+const express = require('express'); // import express
+const htmlRoutes = require('./routes/html'); // import html routes
+const apiRoutes = require('./routes/api'); // import api routes
+const PORT = process.env.PORT || 3001; // set port
+const app = express(); // create express app
+app.use(express.urlencoded({ extended: false }));   // parse incoming data
+app.use(express.json());    // parse incoming data
+app.use(express.static("public")); // serve static files
+app.use(htmlRoutes); // use html routes
+app.use(apiRoutes); // use api routes
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
